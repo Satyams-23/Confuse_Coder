d8421882b68d85b522b96a1a189bdabbf080898d
Satyam Sharma <ss8454015344@gmail.com>
2024-03-27 20:24:44 +0530

    Email Done

---
+++ Signup-Signin-Login-Auth/routes/authRoutes.js
@@ -0,0 +1,17 @@
+
+const UserController = require('../controllers/UserController');
+const express = require('express');
+const router = express.Router();
+
+router.post('/signup', UserController.SignupWithOtp);
+router.post('/signup/verify', UserController.SignupWithOtpVerification);
+router.post('/signin', UserController.Signin);
+router.post('/signout', UserController.Signout);
+router.post('/resend-otp', UserController.resendotpforsignup);
+router.post('/forgot-password', UserController.forgotpassword);
+router.post('/reset-password', UserController.resetpassword);
+router.post('/resend-otp-forgot-password', UserController.resendotpforforgotpassword);
+
+
+module.exports = router;
+
---
+++ Signup-Signin-Login-Auth/utils/generateOTP.js
@@ -0,0 +1,9 @@
+//generate OTP
+
+const generateOTP = async (req, res, next) => {
+    const otp = Math.floor(10000 + Math.random() * 60000);//
+
+    return otp;
+}
+
+module.exports = generateOTP;
